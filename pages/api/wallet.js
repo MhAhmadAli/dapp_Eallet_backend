a7dc196c4d509adf32f4547679a7f8ef24784ecf
Ahmad Ali <ahmad.ali@purelogics.net>
2023-06-08 08:50:33 +0500

    big fix

--- pages/api/wallet.js
+++ pages/api/wallet.js
@@ -1,4 +1,3 @@
-import "@ethersproject/shims"
 import { ethers } from "ethers";
 
 const provider = new ethers.providers.JsonRpcProvider("https://sepolia.infura.io/v3/d03ebd52ffdf4f1e8468e95035e931c3");
